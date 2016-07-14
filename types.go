e28e4e2ecee02a602bfeb1e4077dd5659bf739b4
Jonathan Leibiusky (@xetorthio) <ionathan@gmail.com>
2016-07-14 11:50:28 -0300

    Nicer plan output

--- types.go
+++ types.go
@@ -5,7 +5,6 @@ import (
 	"log"
 	"math"
 	"reflect"
-	"strings"
 
 	"github.com/fatih/color"
 )
@@ -22,19 +21,11 @@ func init() {
 	normal = color.New(color.FgWhite)
 }
 
-func print(c *color.Color, depth int, field, action, msg string) {
-	if field != "" {
-		c.Printf("%s%s%s: %s", action, strings.Repeat(" ", depth*2), field, msg)
-	} else {
-		c.Printf("%s%s%s", action, strings.Repeat(" ", depth*2), msg)
-	}
-}
 func PrintServiceSpecDiff(current, expected interface{}) {
-	_printServiceSpecDiff(0, "", current, expected)
+	_printServiceSpecDiff("", current, expected)
 }
 
-func _printServiceSpecDiff(depth int, field string, current, expected interface{}) {
-	depth++
+func _printServiceSpecDiff(namespace string, current, expected interface{}) {
 	currentType := reflect.TypeOf(current)
 	expectedType := reflect.TypeOf(expected)
 
@@ -49,21 +40,19 @@ func _printServiceSpecDiff(depth int, field string, current, expected interface{
 
 		c := int(math.Max(float64(currentValue.Len()), float64(expectedValue.Len())))
 
-		print(normal, depth, field, "", "[\n")
 		for i := 0; i < c; i++ {
+			newNamespace := fmt.Sprintf("%s[%d]", namespace, i)
 			if i >= currentValue.Len() {
-				_printServiceSpecDiff(depth, "", reflect.Indirect(reflect.New(expectedValue.Index(i).Type())).Interface(), expectedValue.Index(i).Interface())
+				_printServiceSpecDiff(newNamespace, reflect.Indirect(reflect.New(expectedValue.Index(i).Type())).Interface(), expectedValue.Index(i).Interface())
 			} else if i >= expectedValue.Len() {
-				_printServiceSpecDiff(depth, "", currentValue.Index(i).Interface(), reflect.Indirect(reflect.New(currentValue.Index(i).Type())).Interface())
+				_printServiceSpecDiff(newNamespace, currentValue.Index(i).Interface(), reflect.Indirect(reflect.New(currentValue.Index(i).Type())).Interface())
 			} else {
-				_printServiceSpecDiff(depth, "", currentValue.Index(i).Interface(), expectedValue.Index(i).Interface())
+				_printServiceSpecDiff(newNamespace, currentValue.Index(i).Interface(), expectedValue.Index(i).Interface())
 			}
 		}
-		print(normal, depth, "", "", "]\n")
 	case reflect.Map:
 		currentValue := reflect.ValueOf(current)
 		expectedValue := reflect.ValueOf(expected)
-		print(normal, depth, field, "", "{\n")
 
 		for _, k := range currentValue.MapKeys() {
 			ev := expectedValue.MapIndex(k)
@@ -73,7 +62,8 @@ func _printServiceSpecDiff(depth int, field string, current, expected interface{
 			} else {
 				expectedKeyValue = reflect.Indirect(reflect.New(currentValue.MapIndex(k).Type())).Interface()
 			}
-			_printServiceSpecDiff(depth, fmt.Sprintf("%s", k.Interface()), currentValue.MapIndex(k).Interface(), expectedKeyValue)
+			newNamespace := fmt.Sprintf("%s.%s", namespace, k.Interface())
+			_printServiceSpecDiff(newNamespace, currentValue.MapIndex(k).Interface(), expectedKeyValue)
 		}
 
 		for _, k := range expectedValue.MapKeys() {
@@ -84,10 +74,9 @@ func _printServiceSpecDiff(depth int, field string, current, expected interface{
 			} else {
 				currentKeyValue = reflect.Indirect(reflect.New(expectedValue.MapIndex(k).Type())).Interface()
 			}
-			_printServiceSpecDiff(depth, fmt.Sprintf("%s", k.Interface()), currentKeyValue, expectedValue.MapIndex(k).Interface())
+			newNamespace := fmt.Sprintf("%s.%s", namespace, k.Interface())
+			_printServiceSpecDiff(newNamespace, currentKeyValue, expectedValue.MapIndex(k).Interface())
 		}
-
-		print(normal, depth, "", "", "}\n")
 	case reflect.Ptr:
 		currentValue := reflect.ValueOf(current)
 		expectedValue := reflect.ValueOf(expected)
@@ -107,99 +96,30 @@ func _printServiceSpecDiff(depth int, field string, current, expected interface{
 			dev = reflect.Indirect(expectedValue).Interface()
 		}
 
-		_printServiceSpecDiff(depth, "", dcv, dev)
-
-		/*
-			                current          expected
-					nil              nil
-					*time.Duration   nil
-					nil              *time.Duration
-					*time.Duration   *time.Duration
-
-
-
-			                *time.Duration
-					time.Duration
-
-					currentValue := reflect.ValueOf(current)
-					expectedValue := reflect.ValueOf(expected)
-
-					var curr, exp interface{}
-
-					if !reflect.Indirect(currentValue).IsValid() {
-						curr = nil
-					} else {
-						curr = currentValue.Interface()
-					}
-
-					if !reflect.Indirect(expectedValue).IsValid() {
-						curr = nil
-					} else {
-						curr = currentValue.Interface()
-					}
-
-					_printServiceSpecDiff(depth, "", curr, exp)
-
-		*/
+		_printServiceSpecDiff(namespace, dcv, dev)
 	case reflect.Struct:
 		currentValue := reflect.ValueOf(current)
 		expectedValue := reflect.ValueOf(expected)
 
-		first := true
 		for i := 0; i < currentValue.NumField(); i++ {
 			f := currentValue.Type().Field(i)
 			if f.PkgPath == "" {
-				field = f.Name
-				if first {
-					print(normal, depth, field, "", "{\n")
-					first = false
-				}
-				_printServiceSpecDiff(depth, field, currentValue.Field(i).Interface(), expectedValue.Field(i).Interface())
+				newNamespace := fmt.Sprintf("%s.%s", namespace, f.Name)
+				_printServiceSpecDiff(newNamespace, currentValue.Field(i).Interface(), expectedValue.Field(i).Interface())
 			}
-
-		}
-		if !first {
-			print(normal, depth, "", "", "}\n")
 		}
 	default:
 		sc := fmt.Sprint(current)
 		se := fmt.Sprint(expected)
 
 		if sc == se {
-			print(normal, depth, field, "", fmt.Sprintf(`"%s" => "%s"`, sc, se))
+			normal.Printf("  %s:\t\t\t\t\"%s\" => \"%s\"\n", namespace, sc, se)
 		} else if sc == "" {
-			print(green, depth, field, "+", fmt.Sprintf(`"%s"`, se))
+			green.Printf("  %s:\t\t\t\t\"\" => \"%s\"\n", namespace, se)
 		} else if se == "" {
-			print(red, depth, field, "-", fmt.Sprintf(`"%s"`, sc))
+			red.Printf("  %s:\t\t\t\t\"%s\" => \"\"\n", namespace, sc)
 		} else {
-			print(yellow, depth, field, "+/-", fmt.Sprintf(`"%s" => "%s"`, sc, se))
+			yellow.Printf("  %s:\t\t\t\t\"%s\" => \"%s\"\n", namespace, sc, se)
 		}
-		fmt.Print("\n")
 	}
-	/*
-		Image string
-		Labels map[string]string
-		Command string
-		Args []string
-		Env             []string          `json:",omitempty"`
-		Dir             string            `json:",omitempty"`
-		User            string            `json:",omitempty"`
-		Mounts          []Mount           `json:",omitempty"`
-		StopGracePeriod *time.Duration    `json:",omitempty"`
-	*/
 }
-
-/*
-db
- +/- port: 5000 => 6000
- - port: 5000
- + port: 5000
-
-
-
-db
- +/-  port: 5000 => 6000
- +/-  env : ["a","b","c","d"] => ["a","c","b","d"]
-
-
-*/
