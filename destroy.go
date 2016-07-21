764e8f6003724225a64fdf805690a9b03da07e7f
Marcos Lilljedahl <marcosnils@gmail.com>
2016-07-21 20:02:16 -0300

    Add destroy file

---
+++ destroy.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"strings"
+
+	"golang.org/x/net/context"
+
+	"github.com/docker/engine-api/client"
+	"github.com/fatih/color"
+	"github.com/urfave/cli"
+)
+
+func destroy(c *cli.Context) error {
+	bundle, stackName, err := getBundleFromContext(c)
+	if err != nil {
+		return err
+	}
+
+	var services []string
+	for name, _ := range bundle.Services {
+		services = append(services, fmt.Sprintf("%s_%s", stackName, name))
+	}
+
+	force := c.Bool("force")
+
+	swarm, swarmErr := client.NewEnvClient()
+	if swarmErr != nil {
+		return cli.NewExitError(swarmErr.Error(), 3)
+	}
+
+	if !force {
+		fmt.Printf("Are you sure you want to remove the following services? (%s) yes/no: ", strings.Join(services, ","))
+		var input string
+		fmt.Scanln(&input)
+		switch {
+		case input == "no":
+			log.Fatal("Aborting")
+		case input == "yes":
+		default:
+			log.Fatalf("Incorrect option \"%s\", aborting", input)
+		}
+	}
+
+	for _, service := range services {
+		color.Cyan("Removing service %s\n", service)
+		servicesErr := swarm.ServiceRemove(context.Background(), service)
+		if servicesErr != nil {
+			log.Println("Error removing service %s:, %s", service, err)
+		}
+	}
+
+	return nil
+}
